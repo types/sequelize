a328109a84476f26287494a25a3e3e434a4dcb05
Louay Alakkad <louay@alakkad.me>
2016-03-02 14:09:07 +0000

    more tests

--- test/test.ts
+++ test/test.ts
@@ -3,3 +3,10 @@ import * as Sequelize from 'sequelize';
 new Sequelize('str');
 
 var s: Sequelize.Connection;
+var p: Sequelize.Promise<any>;
+var e: typeof Sequelize.Error;
+
+var v: typeof Sequelize.Validator;
+var qt: typeof Sequelize.QueryTypes;
+
+var i: typeof Sequelize.Instance;
