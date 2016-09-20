1df9b26b025e9796e5776639b1a01da4081df1d6
Felix Becker <felix.b@outlook.com>
2016-09-20 18:23:09 +0200

    Remove Promise.sql() (#62)

--- 3/lib/promise.d.ts
+++ 3/lib/promise.d.ts
@@ -1,15 +1,5 @@
 import * as Bluebird from 'bluebird';
 
-/**
- * A slightly modified version of bluebird promises. This means that, on top of the methods below, you can also call all the methods listed on the link below.
- *
- * The main difference is that sequelize promises allows you to attach a listener that will be called with the generated SQL, each time a query is run.
- *
- * The sequelize promise class works seamlessly with other A+/thenable libraries, with one exception.
- * If you want to propagate SQL events across then, all calls etc., you must use sequelize promises exclusively.
- */
-declare class SequelizePromise<T> extends Bluebird<T> {
-  sql(fct: (sql: string) => void): void;
-}
-
-export = SequelizePromise;
+declare const Promise: typeof Bluebird;
+declare type Promise<T> = Bluebird<T>;
+export = Promise;
