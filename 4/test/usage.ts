5057e89963753c357871929c4b4964212a7c5127
Felix Becker <felix.b@outlook.com>
2017-06-07 18:08:14 +0200

    Use workaround for static polymorphic this
    https://github.com/Microsoft/TypeScript/issues/5863#issuecomment-302861175

--- 4/test/usage.ts
+++ 4/test/usage.ts
@@ -1,9 +1,10 @@
 
 import {User, Group} from './models/User';
 
-async function test(): Promise<void> {
+async function test(): Promise<User> {
 
-  const user = await User.findOne({include: [Group]}) as User;
+  const user = await User.findOne({include: [Group]});
+  User.update({  }, { where: {} });
   user.firstName = 'John';
   await user.save();
   await user.setGroup(2);
@@ -11,6 +12,9 @@ async function test(): Promise<void> {
   new User();
   new User({firstName: 'John'});
 
-  const user2 = await User.create({firstName: 'John', groupId: 1}) as User;
+  const user2 = await User.create({firstName: 'John', groupId: 1});
   await User.findAndCountAll({ distinct: true });
+  return user2;
 }
+
+test();
