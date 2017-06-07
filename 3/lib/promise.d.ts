537400ae7cc7d98ccde80f6de35dcabba3963100
Felix Becker <felix.b@outlook.com>
2017-06-07 21:14:23 +0200

    Use branches instead of subfolders

--- 3/lib/promise.d.ts
+++
@@ -1,5 +0,0 @@
-import * as Bluebird from 'bluebird';
-
-declare const Promise: typeof Bluebird;
-declare type Promise<T> = Bluebird<T>;
-export = Promise;
--- 4/lib/sql-string.d.ts
+++
@@ -1,4 +0,0 @@
-export function escapeId(val: string, forbidQualified?: boolean): string;
-export function escape(val: any, timeZone?: string, dialect?: string, format?: string): string;
-export function format(sql: string, values: any[], timeZone?: string, dialect?: string): string;
-export function formatNamedParameters(sql: string, values: any[], timeZone?: string, dialect?: string): string;
