b23d2958dfe003cd13d91d5bc95cfda0297352b8
ppetzold <ppetzold89@gmail.com>
2017-06-10 00:12:24 +0800

    Add QueryTypes

---
+++ lib/query-types.d.ts
@@ -0,0 +1,16 @@
+
+export interface QueryTypes {
+  SELECT: string;
+  INSERT: string;
+  UPDATE: string;
+  BULKUPDATE: string;
+  BULKDELETE: string;
+  DELETE: string;
+  UPSERT: string;
+  VERSION: string;
+  SHOWTABLES: string;
+  SHOWINDEXES: string;
+  DESCRIBE: string;
+  RAW: string;
+  FOREIGNKEYS: string;
+}
