084627bc5b5256081f5fc18024283448ed0d0ea1
Lukas Spieß <lspiess@microsoft.com>
2017-07-17 17:08:48 +0200

    Make tests pass in TypeScript 2.4

--- index.d.ts
+++ index.d.ts
@@ -3001,11 +3001,11 @@ declare module sequelize {
   }
 
   export interface OrOperator {
-    $or: WhereOperators | WhereAttributeHash | Array<WhereOperators | WhereAttributeHash>;
+    $or: WhereOperators | WhereAttributeHash | Array<Array<number> | WhereOperators | WhereAttributeHash>;
   }
 
   export interface AndOperator {
-    $and: WhereOperators | WhereAttributeHash | Array<WhereOperators | WhereAttributeHash>;
+    $and: WhereOperators | WhereAttributeHash | Array<Array<number> | WhereOperators | WhereAttributeHash>;
   }
 
   /**
@@ -3030,7 +3030,7 @@ declare module sequelize {
     | AndOperator
     | OrOperator
     | WhereGeometryOptions
-    | Array<string | number>; // implicit $or
+    | Array<string | number | WhereAttributeHash>; // implicit $or
 
   /**
    * A hash of attributes to describe your search.
