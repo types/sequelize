d54482b248db93970a0179f3c21d4aa2a8cdfddb
Simon Schick <demwizzy@gmail.com>
2017-12-16 01:06:16 +0100

    fix(blob,scope): fix bad typings

--- lib/model.d.ts
+++ lib/model.d.ts
@@ -1440,6 +1440,10 @@ export interface InitOptions extends ModelOptions<any> {
   sequelize: Sequelize;
 }
 
+export interface ModelConstructor {
+  new (...args: any[]): Model;
+}
+
 export abstract class Model {
 
   /** The name of the database table */
@@ -1614,7 +1618,7 @@ export abstract class Model {
    * @return Model A reference to the model, with the scope(s) applied. Calling scope again on the returned
    *     model will clear the previous scope.
    */
-  static scope(options?: string | Array<string> | ScopeOptions | WhereAttributeHash): typeof Model;
+  static scope<M extends ModelConstructor>(this: M, options?: string | Array<string> | ScopeOptions | WhereAttributeHash): M;
 
   /**
    * Search for multiple instances.
